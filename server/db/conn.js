a171a9ab27bd78777b70ad397166489aa5a8f131
Michael Hofer <hoferdev.mi@gmail.com>
2023-10-25 11:35:34 +0200

    change dotenv.config path to config.env in conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import { MongoClient } from 'mongodb';
 
-dotenv.config();
+dotenv.config({ path: 'config.env' });
 
 const connectionString = process.env.ATLAS_URI || '';
 
