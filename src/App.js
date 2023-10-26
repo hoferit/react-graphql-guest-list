504c9b941d67be183a93c865ccb67b1a74345aaf
Michael Hofer <hoferdev.mi@gmail.com>
2023-10-26 10:47:15 +0200

    move ApolloClient to index.js

--- src/App.js
+++ src/App.js
@@ -9,11 +9,6 @@ import {
 import { useEffect, useState } from 'react';
 import styles from './App.module.scss';
 
-const client = new ApolloClient({
-  uri: 'http://localhost:5050/graphql', // Your GraphQL server URL
-  cache: new InMemoryCache(),
-});
-
 const GET_GUESTS = gql`
   query GetGuests {
     guests {
@@ -112,7 +107,6 @@ export default function App() {
     }
   }
 
-  // delete user with API
   async function deleteGuest(id) {
     try {
       const { data } = await deleteGuestMutation({
@@ -132,92 +126,87 @@ export default function App() {
   const handleSubmit = (event) => {
     event.preventDefault();
   };
-  // const handleKeyPress = async (event) => {
-  //   if (event.key === 'Enter') {
-  //     event.preventDefault();
-  //     await createGuest();
-  //     setFirstName('');
-  //     setLastName('');
-  //     await fetchGuests();
-  //   }
-  // };
+  const handleKeyPress = async (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      await createGuest();
+      setFirstName('');
+      setLastName('');
+    }
+  };
 
   return (
-    <ApolloProvider client={client}>
-      <main className={styles.mainContainer}>
+    <main className={styles.mainContainer}>
+      <section>
+        <h1>Guest List</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            First name
+            <input
+              value={firstName}
+              placeholder="First name"
+              disabled={isLoading}
+              onChange={(event) => setFirstName(event.target.value)}
+            />
+          </label>
+          <label>
+            Last name
+            <input
+              value={lastName}
+              placeholder="Last name"
+              disabled={isLoading}
+              onChange={(event) => {
+                setLastName(event.target.value);
+              }}
+              onKeyUp={handleKeyPress}
+            />
+          </label>
+        </form>
+      </section>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
         <section>
-          <h1>Guest List</h1>
-          <form onSubmit={handleSubmit}>
-            <label>
-              First name
-              <input
-                value={firstName}
-                placeholder="First name"
-                disabled={isLoading}
-                onChange={(event) => setFirstName(event.target.value)}
-              />
-            </label>
-            <label>
-              Last name
-              <input
-                value={lastName}
-                placeholder="Last name"
-                disabled={isLoading}
-                onChange={(event) => {
-                  setLastName(event.target.value);
-                }}
-                onKeyUp={handleKeyPress}
-              />
-            </label>
-          </form>
-        </section>
-        {isLoading ? (
-          <div>Loading...</div>
-        ) : (
-          <section>
-            {guests.map((guest) => {
-              return (
-                <div
-                  className={styles.listItem}
-                  key={`guest-${guest.id}`}
-                  data-test-id="guest"
-                >
-                  <div>
-                    <input
-                      aria-label={`${guest.firstName} ${guest.lastName} attending status`}
-                      type="checkbox"
-                      checked={guest.attending}
-                      onChange={() => {
-                        updateGuest(guest.id, guest.attending).catch((error) =>
-                          console.log(error),
-                        );
-                      }}
-                    />
-                    <span>
-                      {guest.attending === true ? 'attending' : 'not attending'}
-                    </span>
-                  </div>
-                  <p>
-                    {guest.firstName} {guest.lastName}
-                  </p>
-
-                  <button
-                    aria-label={`remove ${guest.firstName}${guest.lastName}`}
-                    onClick={() => {
-                      deleteGuest(guest.id).catch((error) =>
+          {guests.map((guest) => {
+            return (
+              <div
+                className={styles.listItem}
+                key={`guest-${guest.id}`}
+                data-test-id="guest"
+              >
+                <div>
+                  <input
+                    aria-label={`${guest.firstName} ${guest.lastName} attending status`}
+                    type="checkbox"
+                    checked={guest.attending}
+                    onChange={() => {
+                      updateGuest(guest.id, guest.attending).catch((error) =>
                         console.log(error),
                       );
                     }}
-                    disabled={isLoading}
-                  >
-                    Remove guest
-                  </button>
+                  />
+                  <span>
+                    {guest.attending === true ? 'attending' : 'not attending'}
+                  </span>
                 </div>
-              );
-            })}
-          </section>
-        )}
-      </main>
-    </ApolloProvider>
+                <p>
+                  {guest.firstName} {guest.lastName}
+                </p>
+
+                <button
+                  aria-label={`remove ${guest.firstName}${guest.lastName}`}
+                  onClick={() => {
+                    deleteGuest(guest.id).catch((error) => console.log(error));
+                  }}
+                  disabled={isLoading}
+                >
+                  Remove guest
+                </button>
+              </div>
+            );
+          })}
+        </section>
+      )}
+    </main>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import './index.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:5050/graphql', // Your GraphQL server URL
+  cache: new InMemoryCache(),
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
 );
