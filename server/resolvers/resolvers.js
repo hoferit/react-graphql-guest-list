0b553eaa4565e3c2b8888ad2b371fea114b89d95
Michael Hofer <hoferdev.mi@gmail.com>
2023-10-29 10:55:51 +0100

    fix schema.gql refactor resolvers.js

--- server/resolvers/resolvers.js
+++ server/resolvers/resolvers.js
@@ -1,43 +1,46 @@
 import { ObjectId } from 'mongodb';
+import connectToDatabase from '../db/conn.js';
+
+const db = await connectToDatabase();
 
 const resolvers = {
   Query: {
-    guests: async (parent, args, { db }) => {
-      return await db.collection('guests').find().toArray();
+    guests: async (_, args, context) => {
+      const collection = await db.collection('guests');
+      return await collection.find().toArray();
     },
   },
   Mutation: {
-    createGuestMutation: async (parent, { firstName, lastName }, { db }) => {
-      try {
-        const result = await db
-          .collection('guests')
-          .insertOne({ firstName, lastName, attending: false });
+    createGuestMutation: async (_, { firstName, lastName }, context) => {
+      const collection = await db.collection('guests');
+      const result = await collection.insertOne({
+        firstName,
+        lastName,
+        attending: false,
+      });
 
-        if (result.ops && result.ops.length > 0) {
-          return result.ops[0];
-        } else {
-          throw new Error('Failed to create a new guest.');
-        }
-      } catch (error) {
-        console.error('Error creating guest:', error);
-        throw error; // Re-throw the error for GraphQL to handle
+      if (result.acknowledged) {
+        return { firstName, lastName, attending: false, id: result.insertedId };
+      } else {
+        throw new Error('Failed to create a new guest.');
       }
     },
-    updateGuestMutation: async (parent, { id, attending }, { db }) => {
-      const result = await db
-        .collection('guests')
-        .findOneAndUpdate(
-          { _id: ObjectId(id) },
-          { $set: { attending } },
-          { returnOriginal: false },
-        );
-      return result.value;
+    updateGuestMutation: async (_, { id, attending }, context) => {
+      const query = { _id: new ObjectId(id) };
+      const collection = await db.collection('guests');
+      const update = await collection.updateOne(query, { $set: { attending } });
+
+      if (update.acknowledged) {
+        return await collection.findOne(query);
+      } else {
+        return null;
+      }
     },
-    deleteGuestMutation: async (parent, { id }, { db }) => {
-      const result = await db
-        .collection('guests')
-        .findOneAndDelete({ _id: ObjectId(id) });
-      return result.value;
+    deleteGuestMutation: async (_, { id }, context) => {
+      const collection = await db.collection('guests');
+      const dbDelete = await collection.deleteOne({ _id: new ObjectId(id) });
+
+      return dbDelete.acknowledged && dbDelete.deletedCount === 1;
     },
   },
 };
