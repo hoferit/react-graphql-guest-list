2c82518d1751bcda02653293385dc01423c84d2e
Michael Hofer <hoferdev.mi@gmail.com>
2023-10-31 13:16:34 +0100

    refactor resolvers.js for use with prisma

--- server/resolvers/resolvers.js
+++ server/resolvers/resolvers.js
@@ -1,63 +1,33 @@
-import { ObjectId } from 'mongodb';
-import connectToDatabase from '../db/conn.js';
+import { PrismaClient } from '@prisma/client';
 
-const db = await connectToDatabase();
+const prisma = new PrismaClient();
 
 const resolvers = {
   Query: {
-    guests: async (_, args, context) => {
-      const collection = await db.collection('guests');
-      const guestList = await collection.find().toArray();
-
-      // Map MongoDB _id to custom id field for GraphQL compatibility
-      return guestList.map((guest) => ({
-        ...guest,
-        id: guest._id.toString(), // Convert ObjectId to a string
-      }));
-    },
+    guests: () => prisma.guest.findMany(),
   },
   Mutation: {
-    createGuestMutation: async (_, { firstName, lastName }, context) => {
-      const collection = await db.collection('guests');
-      const result = await collection.insertOne({
-        firstName,
-        lastName,
-        attending: true,
-      });
-
-      if (result.acknowledged) {
-        return {
+    createGuestMutation: async (_, { firstName, lastName }) => {
+      return prisma.guest.create({
+        data: {
           firstName,
           lastName,
-          attending: false,
-          id: result.insertedId.toString(),
-        };
-      } else {
-        throw new Error('Failed to create a new guest.');
-      }
+          attending: true,
+        },
+      });
     },
-    updateGuestMutation: async (_, { id, attending }, context) => {
-      const query = { _id: new ObjectId(id) };
-      const collection = await db.collection('guests');
-      const update = await collection.updateOne(query, { $set: { attending } });
-
-      if (update.acknowledged) {
-        const updatedGuest = await collection.findOne(query);
-        return {
-          id: updatedGuest._id, // Map the _id to id
-          firstName: updatedGuest.firstName,
-          lastName: updatedGuest.lastName,
-          attending: updatedGuest.attending,
-        };
-      } else {
-        return null;
-      }
+    updateGuestMutation: async (_, { id, attending }) => {
+      return prisma.guest.update({
+        where: { id },
+        data: {
+          attending,
+        },
+      });
     },
-    deleteGuestMutation: async (_, { id }, context) => {
-      const collection = await db.collection('guests');
-      const dbDelete = await collection.deleteOne({ _id: new ObjectId(id) });
-
-      return dbDelete.acknowledged && dbDelete.deletedCount === 1;
+    deleteGuestMutation: async (_, { id }) => {
+      return prisma.guest.delete({
+        where: { id },
+      });
     },
   },
 };
